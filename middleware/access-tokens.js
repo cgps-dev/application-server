abdb44dd2e8b036452cfd5642eb7659e95ca7531
Khalil <ka10@sanger.ac.uk>
2021-02-15 19:12:09 +0000

    Update access-tokens.js

--- middleware/access-tokens.js
+++ middleware/access-tokens.js
@@ -1,20 +1,17 @@
 module.exports = function (options) {
   return (req, res, next) => {
-    if (!req.user) {
-      const token = (req.headers["access-token"] || req.query["access-token"]);
-      if (typeof token === "string") {
-        options.getUser(token)
-          .then((user) => {
-            if (user) {
-              req.user = user;
-            }
-            next();
-          })
-          .catch(next);
-      }
+    const token = (req.headers["access-token"] || req.query["access-token"]);
+    if (req.user || typeof token !== "string") {
+      return next();
     }
-    else {
-      next();
-    }
+
+    options.getUser(token)
+      .then((user) => {
+        if (user) {
+          req.user = user;
+        }
+        next();
+      })
+      .catch(next);
   };
-}
+};
