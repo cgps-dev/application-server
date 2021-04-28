c6319fc3d8ffd253727858ff3f431b6caec06679
Khalil <ka10@sanger.ac.uk>
2021-04-28 12:56:05 +0100

    Update get-user.js

--- middleware/get-user.js
+++ middleware/get-user.js
@@ -2,20 +2,24 @@ const { getSession } = require('next-auth/client');
 const { ApiError } = require('next/dist/next-server/server/api-utils');
 
 async function getUserFromAccessToken(req, res) {
-  const jwt = require("next-auth/dist/lib/jwt").default;
-
-  const getConfig = require("next/config").default;
-  const { serverRuntimeConfig } = getConfig();
-
   const accessToken = req.headers["access-token"];
 
   if (accessToken) {
-    try {
+      const jwt = require("next-auth/dist/lib/jwt").default;
+      const adapters = require('next-auth/dist/adapters').default;
+      const getConfig = require("next/config").default;
+      const { serverRuntimeConfig } = getConfig();
+  
+      try {
       const user = await jwt.decode({
         secret: serverRuntimeConfig.secret,
         token: accessToken,
       });
-      return user || undefined;
+      const userId = user.id;
+      const adapter = adapters.Default(serverRuntimeConfig.auth.database)
+      const { getUser } = await adapter.getAdapter({ logger: console });
+      const userDoc = await getUser(userId);
+      return userDoc || undefined;
     }
     catch (err) {
       if (err?.code === "ERR_JWS_VERIFICATION_FAILED") {
