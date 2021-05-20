92377dbd131773a5e0917ec4c17452d361c58fe5
Khalil <ka10@sanger.ac.uk>
2021-05-20 14:59:51 +0100

    Create get-database-adapter.js

---
+++ nextjs/auth/get-database-adapter.js
@@ -0,0 +1,8 @@
+module.exports = async function getDatabaseAdapter(req, res) {
+  const adapters = require('next-auth/dist/adapters').default;
+  const getConfig = require("next/config").default;
+  const { serverRuntimeConfig } = getConfig();
+  const adapter = adapters.Default(serverRuntimeConfig.auth.database)
+  const dbAdapter = await adapter.getAdapter({ logger: console });
+  return dbAdapter;
+}
