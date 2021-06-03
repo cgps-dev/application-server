5b522d22f9e437d354390aab7e6a3cc695a57dda
Khalil <ka10@sanger.ac.uk>
2021-06-03 18:48:27 +0100

    Update with-custom-css.js

--- nextjs/auth/with-custom-css.js
+++ nextjs/auth/with-custom-css.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const renderToString = require("preact-render-to-string");
 
 const signin = require(path.join(process.cwd(), "node_modules/next-auth/dist/server/pages/signin")).default;
 const signout = require(path.join(process.cwd(), "node_modules/next-auth/dist/server/pages/signout")).default;
@@ -8,19 +9,39 @@ const css = require(path.join(process.cwd(), "node_modules/next-auth/dist/css/in
 const renderPageModule = require(path.join(process.cwd(), "node_modules/next-auth/dist/server/pages/index.js"));
 
 renderPageModule.default = (req, res) => {
-  const { baseUrl, basePath, callbackUrl, csrfToken, providers, theme } = req.options;
+  const { baseUrl, basePath, callbackUrl, csrfToken, providers, theme } = req.options
 
-  res.setHeader("Content-Type", "text/html");
-  function send(html) {
-    res.send(`<!DOCTYPE html><head><style type="text/css">${css()}</style><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="/styles/nextjs-auth-page.css" /></head><body class="__next-auth-theme-${theme}"><div class="page">${html}</div></body></html>`);
+  res.setHeader('Content-Type', 'text/html')
+  function send ({ html, title }) {
+    res.send(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>${css()}</style><link rel="stylesheet" href="/styles/nextjs-auth-page.css" /><title>${title}</title></head><body class="__next-auth-theme-${theme}"><div class="page">${renderToString(html)}</div></body></html>`)
   }
 
   return {
-    signin(props) { send(signin({ csrfToken, providers, callbackUrl, ...req.query, ...props })); },
-    signout(props) { send(signout({ csrfToken, baseUrl, basePath, ...props })); },
-    verifyRequest(props) { send(verifyRequest({ baseUrl, ...props })); },
-    error(props) { send(error({ basePath, baseUrl, res, ...props })); },
-  };
-};
+    signin (props) {
+      send({
+        html: signin({ csrfToken, providers, callbackUrl, ...req.query, ...props }),
+        title: 'Sign In'
+      })
+    },
+    signout (props) {
+      send({
+        html: signout({ csrfToken, baseUrl, basePath, ...props }),
+        title: 'Sign Out'
+      })
+    },
+    verifyRequest (props) {
+      send({
+        html: verifyRequest({ baseUrl, ...props }),
+        title: 'Verify Request'
+      })
+    },
+    error (props) {
+      send({
+        html: error({ basePath, baseUrl, res, ...props }),
+        title: 'Error'
+      })
+    }
+  }
+}
 
 module.exports = function() {};
