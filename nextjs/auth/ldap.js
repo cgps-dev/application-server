cf0a8576c61b9d37a003da38955bf89a6b85631d
Khalil <ka10@sanger.ac.uk>
2022-01-05 15:22:33 +0000

    https://gitlab.com/cgps/cdc/sars-cov-2/-/issues/16

--- nextjs/auth/ldap.js
+++ nextjs/auth/ldap.js
@@ -18,6 +18,11 @@ function login(config, credentials) {
           resolve(null);
         }
         else {
+          if (config.groupDn) {
+            if (!profile._groups.some((x) => x.dn === config.groupDn)) {
+              resolve(null);
+            }
+          }
           resolve(profile);
         }
       },
