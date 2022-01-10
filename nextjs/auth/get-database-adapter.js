c60a281d7eff45db3cb11b258c922ed7f3d7277c
Khalil <ka10@sanger.ac.uk>
2022-01-10 08:00:41 +0000

    Update get-database-adapter.js

--- nextjs/auth/get-database-adapter.js
+++ nextjs/auth/get-database-adapter.js
@@ -2,7 +2,7 @@ module.exports = async function getDatabaseAdapter(req, res) {
   const adapters = require("next-auth/adapters").default;
   const getConfig = require("next/config").default;
   const { serverRuntimeConfig } = getConfig();
-  const adapter = adapters.Default(serverRuntimeConfig.auth.database)
+  const adapter = adapters.Default(serverRuntimeConfig.auth.database);
   const dbAdapter = await adapter.getAdapter({
     logger: console,
     secret: serverRuntimeConfig.auth.secret,
